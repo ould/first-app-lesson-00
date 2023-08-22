788311b3e6f1ab974828181038a97b583f7a783d
DenisMac <denis.hellal@codeworks.fr>
2023-08-22 15:14:26 +0200

    phase 11 : routes et parametres

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,20 +1,26 @@
 import { Component, Input  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
+import { RouterLink, RouterOutlet } from  '@angular/router';
+import routeConfig from '../routes';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   template: `
   <section class="listing">
-  <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-  <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-  <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
-</section>
+    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+  </section>
   `,
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-  @Input() housingLocation!: HousingLocation;// en gros l'input qu'il recoit de house, on met ! pour dire que pas besoin d'initialiser on aura un objet du parent de toute facon
+  @Input() housingLocation!: HousingLocation;// en gros l'input qu'il recoit du parent, on met ! pour dire que pas besoin d'initialiser on aura un objet du parent de toute facon
 }
+
+
+//routerLink is an array with two entries: the static portion of the path and the dynamic data
