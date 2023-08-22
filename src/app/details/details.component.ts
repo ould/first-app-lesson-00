c4866dd445b33212d3a86195e989808544ef9a8d
DenisMac <denis.hellal@codeworks.fr>
2023-08-22 16:41:53 +0200

    feat: phase finale

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -66,8 +66,10 @@ export class DetailsComponent {
   housingLocation: HousingLocation | undefined;
 
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+  this.housingService.getHousingLocationById(housingLocationId).then(res => {
+    this.housingLocation = res;
+  });
   }
 
   submitApplication() {
